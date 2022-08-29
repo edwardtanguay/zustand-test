a95b05120f5d3b7e5c5ff39f78cb828e6f8d2cd3
Edward Tanguay (windows) <edward@tanguay.info>
2022-08-29 17:50:16 +0200

    load api test with error

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { useStore, AppState } from './appStore';
 import './App.scss';
 import { First } from './components/First';
+import { LoadAppData } from './appStore';
 
 function App() {
 	const count = useStore((state: AppState) => state.count);
+	const techBooks = useStore((state: AppState) => state.techBooks);
 	const increaseCount = useStore((state: AppState) => state.increaseCount);
 
 	return (
@@ -13,6 +15,9 @@ function App() {
 			<button onClick={increaseCount}>increase</button>
 			<hr />
 			<First />
+			<hr />
+			<LoadAppData />
+			<p>{techBooks.length}</p>
 		</div>
 	);
 }
--- src/appStore.tsx
+++ src/appStore.tsx
@@ -1,13 +1,52 @@
+import { useEffect } from 'react';
+import axios from 'axios';
 import create from 'zustand';
+
+const url = 'https://edwardtanguay.netlify.app/share/techBooks.json';
 
 export interface AppState {
 	search: string;
 	count: number;
 	increaseCount: () => void;
+	techBooks: any[];
+	loadTechBooks: (techBooks: any[]) => void;
 }
 
 export const useStore = create<AppState>((set) => ({
 	search: '',
 	count: 3,
 	increaseCount: () => set((state: AppState) => ({ count: state.count + 1 })),
+	techBooks: [],
+	loadTechBooks: (techBooks: any[]) =>
+		set((state: AppState) => ({
+			techBooks: (state.techBooks = techBooks),
+		})),
 }));
+
+export const LoadAppData = () => {
+	const loadTechBooks = (techBooks: any[]) =>  useStore((state: AppState) => state.techBooks = techBooks);
+	const increaseCount = useStore((state: AppState) => state.increaseCount);
+	// useEffect(() => {
+	// 	(async () => {
+	// 		const _techBooks = (await axios.get(url)).data;
+	// 		console.log(_techBooks);
+	// 		// loadTechBooks(_techBooks);
+	// 	})();
+	// }, []);
+
+	const handleLoadTechBooks = async () => {
+		(async () => {
+			const _techBooks = (await axios.get(url)).data;
+			console.log(_techBooks);
+			loadTechBooks(_techBooks);
+			increaseCount();
+		})();
+	}
+	return (
+		<>
+			<p>nnn</p>
+			<button onClick={increaseCount}>increase</button>
+			<button onClick={handleLoadTechBooks}>load tech books</button>
+		</>
+	);
+};
