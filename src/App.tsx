70ac897f23e2e87eb12ae1b5252b31da7122b20c
Edward Tanguay (windows) <edward@tanguay.info>
2022-08-29 22:00:08 +0200

    left/right works

--- src/App.tsx
+++ src/App.tsx
@@ -8,42 +8,75 @@ function App() {
 	return (
 		<div className="App">
 			<h1>Zustand Test</h1>
-			<div className="data">
-				<label>Message:</label>
-				<input
-					type="text"
-					value={store.message}
-					onChange={(e) => store.setMessage(e.target.value)}
-				/>
-			</div>
 
-			<div className="data">
-				<label>Colors:</label>
-				<div>
-					<button onClick={() => store.addColor('blue')}>blue</button>
-					<button onClick={() => store.addColor('red')}>red</button>
-					<button onClick={() => store.addColor('yellow')}>yellow</button>
-				</div>
-				<button onClick={() => store.deleteColor()}>Delete color</button>
-				<button onClick={() => store.setColors([])}>Delete all colors</button>
-			</div>
+			<main>
+				<section className="controlArea">
+					<div className="data">
+						<label>Message:</label>
+						<input
+							type="text"
+							value={store.message}
+							onChange={(e) => store.setMessage(e.target.value)}
+						/>
+					</div>
 
-			<div className="data">
-				<label>Object:</label>
-				<div>
-					<button onClick={() => store.toggleCurrentUserStatusOnline()}>toggle online status</button>
-					<button onClick={() => store.toggleCurrentUserStatusEmail()}>toggle email status</button>
-				</div>
-			</div>
+					<div className="data">
+						<label>Colors:</label>
+						<div>
+							<button onClick={() => store.addColor('blue')}>
+								blue
+							</button>
+							<button onClick={() => store.addColor('red')}>
+								red
+							</button>
+							<button onClick={() => store.addColor('yellow')}>
+								yellow
+							</button>
+						</div>
+						<button onClick={() => store.deleteColor()}>
+							Delete color
+						</button>
+						<button onClick={() => store.setColors([])}>
+							Delete all colors
+						</button>
+					</div>
 
-			<div className="data">
-				<label>Change multiple values:</label>
-				<div>
-					<button onClick={() => store.deleteVowelsAndColorRed()}>delete vowels from message and color red from colors</button>
-				</div>
-			</div>
+					<div className="data">
+						<label>Object:</label>
+						<div>
+							<button
+								onClick={() =>
+									store.toggleCurrentUserStatusOnline()
+								}
+							>
+								toggle online status
+							</button>
+							<button
+								onClick={() =>
+									store.toggleCurrentUserStatusEmail()
+								}
+							>
+								toggle email status
+							</button>
+						</div>
+					</div>
 
-			<InfoBox />
+					<div className="data">
+						<label>Change multiple values:</label>
+						<div>
+							<button
+								onClick={() => store.deleteVowelsAndColorRed()}
+							>
+								delete vowels from message and color red from
+								colors
+							</button>
+						</div>
+					</div>
+				</section>
+				<section className="dataArea">
+					<InfoBox />
+				</section>
+			</main>
 		</div>
 	);
 }
